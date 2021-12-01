b04998734d95d7a59f369924fe41c32cd57068bc
erfanpaya2021 <erfanpaya2021@gmail.com>
2021-12-01 22:47:35 +0330

    Display questions

---
+++ js/game.js
@@ -0,0 +1,85 @@
+const question = document.querySelector(".question");
+const choices = document.querySelectorAll(".choice-text");
+
+let currentQuestion = {};
+let accesptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1,
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3,
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4,
+  },
+];
+
+// CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+const startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+const getNewQuestion = () => {
+  // GO TO END PAGE
+  if (availableQuestions.length === 0 || questionCounter === MAX_QUESTIONS) {
+    window.location.assign("../end.html");
+  }
+
+  // GET QUESTION FROM AVAILABLE QUESTION LIST
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+
+  // DISPLAY QUESTION
+  question.innerText = currentQuestion["question"];
+
+  // DISPLAY CHOICES
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  // REMOVE CURRENT QUESTION FROM AVAILABE QUESTION LIST
+  availableQuestions.splice(questionIndex, 1);
+  accesptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!accesptingAnswers) return;
+
+    accesptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    getNewQuestion();
+  });
+});
+
+startGame();
